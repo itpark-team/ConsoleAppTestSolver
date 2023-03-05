0e1669f94b957068f99cb79aa281922487d41090
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2023-03-05 18:27:38 +0200

    LoadExamples

---
+++ ConsoleAppTestSolver/TestExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestSolver
+{
+    internal class TestExample
+    {
+        public string Question { get; set; }
+
+        public List<string> Answers { get; set; }
+
+        public int IndexRightAnswers { get; set; }
+    }
+}
---
+++ ConsoleAppTestSolver/TestsPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestSolver
+{
+    internal class TestsPlayer
+    {
+        private List<TestExample> _testExamples;
+
+        public TestsPlayer()
+        {
+            _testExamples = new List<TestExample>();
+        }
+
+        public void LoadFromFile(string fileName)
+        {
+            StreamReader reader = new StreamReader(fileName);
+            int examplesCount = int.Parse(reader.ReadLine());
+            
+            for (int i = 0; i < examplesCount; i++)
+            {
+                string question = reader.ReadLine();
+                int countAnswers = int.Parse(reader.ReadLine());
+
+                List<string> answers = new List<string>();
+
+                for (int j = 0; j < countAnswers; j++)
+                {
+                    answers.Add(reader.ReadLine());
+                }
+
+                int indexRightAnswer = int.Parse(reader.ReadLine());
+
+
+                _testExamples.Add(new TestExample()
+                {
+                    Question = question,
+                    Answers = answers,
+                    IndexRightAnswers = indexRightAnswer
+                }) ;
+            }
+
+            reader.Close();
+        }
+
+    }
+}
